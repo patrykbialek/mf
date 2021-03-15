5bf3ba49ca7ba9ed2315d8bac6d120b4909a977f
Patryk Bialek <patryk.bialek@contractors.roche.com>
2021-03-15 18:29:41 +0100

    fix imports

--- projects/mfe1/src/app/app.module.ts
+++ projects/mfe1/src/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { MfeApiService } from '@shared-lib';
+import { MfeApiService, SharedLibModule } from '@shared-lib';
 
 import { AppComponent } from './app.component';
 import { MFE1_ROUTES } from './app.routes';
@@ -11,7 +11,8 @@ import { PatientsComponent } from './patients.component';
 @NgModule({
   imports: [
     BrowserModule,
-    RouterModule.forChild(MFE1_ROUTES)
+    RouterModule.forChild(MFE1_ROUTES),
+    SharedLibModule,
   ],
   declarations: [
     AppComponent,
